c33181acf9cb48e2f32d636b2607eb31b99eb1d4
Nick Moyer <moyer.n.p@gmail.com>
2020-09-07 19:58:37 -0500

    Created migration

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Nick',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 30, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 31, 2020'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
